7237da1bc21c7fd6b2364ec48b0c4fc6a66b20a7
Victor Henrique <68289673+VictorHenrique317@users.noreply.github.com>
2023-10-20 21:58:37 -0300

    First version ready

--- src-tauri/src/model/analysis/metrics/coordinates.rs
+++ src-tauri/src/model/analysis/metrics/coordinates.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case)]
 
 use std::{collections::HashMap, sync::{Mutex, Arc}};
+use nalgebra::{DMatrix, DVector};
 use ndarray::{IxDynImpl, Dim, ArrayD, Array, Array2};
 use numpy::{PyArray2, IntoPyArray};
 use pyo3::{Python, types::PyDict};
@@ -27,7 +28,7 @@ impl Coordinates {
         };
     }
 
-    fn buildDissimilarityMatrix<T: DistancesTrait>(distances: &T, n: usize) -> Array2<f64> {
+    fn buildDissimilarityMatrix<T: DistancesTrait>(distances: &T, n: usize) -> DMatrix<f64> {
         let size: Vec<usize> = vec![n, n];
         let distance_matrix: Arc<Mutex<ArrayD<f64>>> = Arc::new(Mutex::new(Array::zeros(Dim(size.clone())).into_dyn()));
 
@@ -44,52 +45,103 @@ impl Coordinates {
             }
         });
 
-        let squared_proximity_matrix: Array2<f64> = distance_matrix.lock().unwrap().clone().into_shape((n, n)).unwrap();
-        return squared_proximity_matrix;
-    }
-
-    fn SklearnMDS(dissimilarity_matrix: Array2<f64>) -> HashMap<u32, (f64, f64)>{
-        pyo3::prepare_freethreaded_python();
-        let xy_matrix: Array2<f64> = Python::with_gil(|py| {
-            let sklearn = py.import("sklearn.manifold").unwrap();
-
-            let kwargs = PyDict::new(py);
-            kwargs.set_item("dissimilarity", "precomputed").unwrap();
-            kwargs.set_item("normalized_stress", false).unwrap();
-            kwargs.set_item("random_state", 1).unwrap();
-            kwargs.set_item("n_components", 2).unwrap();
-            // kwargs.set_item("n_jobs", -1).unwrap();
-            let mds = sklearn.getattr("MDS").unwrap()
-                .call((), Some(kwargs))
-                .unwrap();
-
-            let dissimilarities_py: &PyArray2<f64> = dissimilarity_matrix.into_pyarray(py);
-            let embedding: &PyArray2<f64> = mds.call_method1("fit_transform", (dissimilarities_py,)).unwrap().extract().unwrap();
-            embedding.readonly().as_array().to_owned()
+        let dissimilarity_matrix: DMatrix<f64> = DMatrix::from_fn(n, n, |i, j| {
+            let index: Dim<IxDynImpl> = Dim(vec![i, j]);
+            let distance_matrix_lock = distance_matrix.lock().unwrap();
+            let matrix_value = distance_matrix_lock.get(index).unwrap();
+            return *matrix_value;
         });
 
-        let n_rows = xy_matrix.shape()[0];
-        let mut xys: HashMap<u32, (f64, f64)> = HashMap::new();
-        for i in 0..n_rows {
-            let xy_row = xy_matrix.row(i);
-            let identifier = (i + 1) as u32;
-            let x = *xy_row.get(0).unwrap() ;
-            let y = *xy_row.get(1).unwrap();
-            xys.insert(identifier, (x, y));
-        }
-        return xys;
+        return dissimilarity_matrix;
     }
 
-    pub fn fitTransformMDS<T: DistancesTrait>(distances: &T, identifier_mapper: &IdentifierMapper) -> HashMap<u32, (f64, f64)>{
-        println!("  Applying Multi Dimensional Scaling...");
-        let n: usize = identifier_mapper.length() as usize;
-        let dissimilarity_matrix = Coordinates::buildDissimilarityMatrix(distances, n);
-        let xys = Coordinates::SklearnMDS(dissimilarity_matrix);
+    // fn SklearnMDS(dissimilarity_matrix: Array2<f64>) -> HashMap<u32, (f64, f64)>{
+    //     pyo3::prepare_freethreaded_python();
+    //     let xy_matrix: Array2<f64> = Python::with_gil(|py| {
+    //         let sklearn = py.import("sklearn.manifold").unwrap();
+
+    //         let kwargs = PyDict::new(py);
+    //         kwargs.set_item("dissimilarity", "precomputed").unwrap();
+    //         kwargs.set_item("normalized_stress", false).unwrap();
+    //         kwargs.set_item("random_state", 1).unwrap();
+    //         kwargs.set_item("n_components", 2).unwrap();
+    //         // kwargs.set_item("n_jobs", -1).unwrap();
+    //         let mds = sklearn.getattr("MDS").unwrap()
+    //             .call((), Some(kwargs))
+    //             .unwrap();
+
+    //         let dissimilarities_py: &PyArray2<f64> = dissimilarity_matrix.into_pyarray(py);
+    //         let embedding: &PyArray2<f64> = mds.call_method1("fit_transform", (dissimilarities_py,)).unwrap().extract().unwrap();
+    //         embedding.readonly().as_array().to_owned()
+    //     });
+
+    //     let n_rows = xy_matrix.shape()[0];
+    //     let mut xys: HashMap<u32, (f64, f64)> = HashMap::new();
+    //     for i in 0..n_rows {
+    //         let xy_row = xy_matrix.row(i);
+    //         let identifier = (i + 1) as u32;
+    //         let x = *xy_row.get(0).unwrap() ;
+    //         let y = *xy_row.get(1).unwrap();
+    //         xys.insert(identifier, (x, y));
+    //     }
+    //     return xys;
+    // }
+
+    fn mds(distances: DMatrix<f64>, dimensions: usize) -> HashMap<u32, (f64, f64)> {
+        let mut m = distances.map(|x| -0.5 * x.powi(2));
+    
+        let row_means = m.row_mean();
+        let col_means = m.column_mean();
+        let total_mean = m.mean();
+    
+        for i in 0..m.nrows() {
+            for j in 0..m.ncols() {
+                m[(i, j)] += total_mean - row_means[i] - col_means[j];
+            }
+        }
+    
+        let svd = m.svd(true, true);
+        let mut singular_values = svd.singular_values.map(|x| x.sqrt());
+        let mut u = svd.u.unwrap();
+    
+        // Create a vector of indices from 0 to n
+        let mut indices: Vec<usize> = (0..singular_values.len()).collect();
+    
+        // Sort the indices by singular values in descending order
+        indices.sort_by(|&i, &j| singular_values[j].partial_cmp(&singular_values[i]).unwrap());
+    
+        // Reorder singular values and U matrix using sorted indices
+        let singular_values_vec: Vec<f64> = indices.iter().map(|&i| singular_values[i]).collect();
+        singular_values = DVector::from_vec(singular_values_vec);
+    
+        let u_vec: Vec<DVector<f64>> = indices.iter().map(|&i| u.column(i).into_owned()).collect();
+        u = DMatrix::from_columns(&u_vec);
+    
+        let mut result = DMatrix::zeros(u.nrows(), dimensions);
+    
+        for i in 0..u.nrows() {
+            for j in 0..dimensions {
+                result[(i, j)] = u[(i, j)] * singular_values[j];
+            }
+        }
+        
+        let mut xys: HashMap<u32, (f64, f64)> = HashMap::new();
+        for i in 0..result.nrows() {
+            let point: Vec<f64> = result.row(i).iter().cloned().collect();
+            let x = point[0];
+            let y = point[1];
+            xys.insert((i + 1) as u32, (x, y)); //TODO: Wrong identifiers?
+        }
+
         return xys;
     }
 
     fn calculate<T: DistancesTrait>(identifier_mapper: &IdentifierMapper, distances: &T) -> HashMap<u32, (f64, f64)> {
-        return Coordinates::fitTransformMDS(distances, &identifier_mapper);
+        println!("  Applying Multi Dimensional Scaling...");
+        let n: usize = identifier_mapper.length() as usize;
+        let dissimilarity_matrix: DMatrix<f64> = Coordinates::buildDissimilarityMatrix(distances, n);
+        let xys = Coordinates::mds(dissimilarity_matrix, 2);
+        return xys;
     }
 }
 
