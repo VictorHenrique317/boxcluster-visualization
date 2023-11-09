5f9bc414eddf1b42e2738b6bc591464c615658b5
Victor <victor.henrique5800@gmail.com>
2023-11-09 12:31:01 -0300

    Switching to d3

--- src/app/dag/dag.component.ts
+++ src/app/dag/dag.component.ts
@@ -28,13 +28,11 @@ export class DagComponent implements AfterViewInit{
   private subscribed_datapoints: Array<DataPoint>;
   
   @ViewChild('vizualization_div') vizualization_div: ElementRef<HTMLDivElement>;
-  private d3_svg: any;
-  private width: number;
-  private height: number;
-  private x: any;
-  private y: any;
-  private xAxis: any;
-  private yAxis: any;
+  private svg: any;
+  private y_correction = 70;
+  private plot_padding = 50;
+  private x_scale: any;
+  private y_scale: any;
 
   private totalDx = 0;
   private totalDy = 0;
@@ -56,90 +54,123 @@ export class DagComponent implements AfterViewInit{
   constructor(private svg_service: SvgService, private dagview_service: DagViewService){
     this.datapoints_subscription = this.dagview_service.datapoints$.subscribe(value => {
       this.subscribed_datapoints = value;
-      // this.drawDataPoints();
+      this.drawDataPoints();
     });
   }
   
   ngAfterViewInit(){
-    // this.width = 1024;
-    this.width = 1024;
-    // this.height = 720;
-    this.height = 720;
+    this.dagview_service.updateDataPoints();
+
+    let width = 1024;
+    let height = 720;
   
-    this.d3_svg = d3.select(this.vizualization_div.nativeElement)
+    this.svg = d3.select(this.vizualization_div.nativeElement)
       .append('svg')
-        .attr('width', this.width)
-        .attr('height', this.height);
+        .attr('width', width)
+        .attr('height',height);
 
-
-    let x = d3.scaleLinear()
-      .domain([0, 1])
-      .range([0, this.width]);
-
-    let x_axis = d3.axisBottom(x);
-
-    this.d3_svg.append('g')
-      .attr('transform', `translate(0,${0})`)
-      .call(x_axis);
-
-    let y = d3.scaleLinear()
-      .domain([1, 0])
-      .range([this.height, 0]);
-
-    let y_axis = d3.axisRight(y);
-
-    this.d3_svg.append('g')
-      .attr('transform', `translate(0,${0})`)
-      .call(y_axis);
-
-    this.x = x;
-    this.y = y;
-    this.xAxis = x_axis;
-    this.yAxis = y_axis;
-
-    this.updateChart();
+    this.updatePlot(width, height);
   }
 
-  updateChart() {
-    // console.log(this.svg_element.nativeElement.clientHeight);
+  private drawGridLines(width: number, height: number){
+    // Define the gridlines
+    let xGridlines = d3.axisBottom(this.x_scale)
+      .tickSize(-height + 2 * this.plot_padding)
+      .tickFormat(() => "");
+
+    let yGridlines = d3.axisLeft(this.y_scale)
+      .tickSize(-width + 2 * this.plot_padding)
+      .tickFormat(() => "");
+
+    // Add the X gridlines
+    this.svg.append("g")			
+      .attr("class", "grid")
+      .attr("transform", "translate(0," + (height - this.plot_padding) + ")")
+      .call(xGridlines);
+
+    // Add the Y gridlines
+    this.svg.append("g")			
+      .attr("class", "grid")
+      .attr("transform", "translate(" + this.plot_padding + ",0)")
+      .call(yGridlines);
+  }
+
+  private updatePlot(width: number, height: number) {
+    d3.select('svg').selectAll('*').remove();
+
+    this.svg
+      .attr('width', width)
+      .attr('height', height);
+
+      // X axis
+    let x_scale = d3.scaleLinear()
+      .domain([-1, 1])
+      .range([this.plot_padding, width - this.plot_padding]);
+
+    let x_axis = d3.axisBottom(x_scale);
+
+    // Y axis
+    let y_scale = d3.scaleLinear()
+      .domain([-1, 1])
+      .range([height - this.y_correction - this.plot_padding, this.plot_padding]);
+
+    let y_axis = d3.axisRight(y_scale);
+
+    this.svg.append('g')
+      .attr('transform', `translate(0,${(height - this.y_correction)/2})`)
+      .call(x_axis);
+
+    this.svg.append('g')
+      .attr('transform', `translate(${width/2},0)`)
+      .call(y_axis);
+
+    this.x_scale = x_scale;
+    this.y_scale = y_scale;
+
+    this.drawGridLines(width, height);
+    this.drawDataPoints();
   }
 
   public onResize(event) {
-    this.width = this.dagWindow.nativeElement.clientWidth;
-    this.height = this.dagWindow.nativeElement.clientHeight;
+    let width = this.dagWindow.nativeElement.clientWidth;
+    let height = this.dagWindow.nativeElement.clientHeight;
 
-    this.updateChart();
+    this.updatePlot(width, height);
   }
 
   ngOnDestroy(){
     this.datapoints_subscription.unsubscribe();
   }
 
-  // private scaleToFitCanvas(x: number, y:number, radius: number){
-  //   // x goes from -1 to 1, and y goes from -1 to 1
-  //   let scaled_x = (x + 1) * this.canvas.nativeElement.width / 2;
-  //   let scaled_y = (y + 1) * this.canvas.nativeElement.height / 2;
-  //   let scaled_radius = radius;
+  private scaleToFitPlot(datapoints: Array<DataPoint>) {
+    let x_max_module = Math.max(...datapoints.map(datapoint => Math.abs(datapoint.x)));
+    let y_max_module = Math.max(...datapoints.map(datapoint => Math.abs(datapoint.y)));
 
-  //   return {x: scaled_x, y: scaled_y, radius: scaled_radius};
-  // }
+    let scaled_datapoints: Array<DataPoint> = datapoints;
+    let scale_x: boolean = x_max_module > 1;
+    let scale_y: boolean = y_max_module > 1;
+    datapoints.forEach(datapoint => {
+        if(scale_x){ datapoint.x /= x_max_module; }
+        if(scale_y){ datapoint.y /= y_max_module; }
+    });
 
-//   private drawDataPoints(){
-//     this.context.save();
+    return scaled_datapoints;
+  }
 
-//     this.context.translate(this.totalDx, this.totalDy);
-//     this.context.scale(this.scale, this.scale);
-//     this.canvas_service.drawGrid(this.canvas, this.maximum_dx*4, this.maximum_dy*4, 10);
-    
-//     for (let i = 0; i < this.subscribed_datapoints.length; i++){
-//       let datapoint = this.subscribed_datapoints[i];
-//       let scaled_datapoint = this.scaleToFitCanvas(datapoint.x, datapoint.y, datapoint.size);
-//       let color: Color = {r: datapoint.r, g: datapoint.g, b: datapoint.b};
-//       this.canvas_service.drawCircle(this.canvas, scaled_datapoint.x, scaled_datapoint.y, scaled_datapoint.radius/2, color);
-//     }
+  private drawDataPoints() {
+    this.svg.selectAll('circle').remove();
+    let scaled_datapoints = this.scaleToFitPlot(this.subscribed_datapoints);
   
-//     this.context.restore();
-//   } 
+    this.svg.selectAll('circle')
+      .data(scaled_datapoints)
+      .enter()
+      .append('circle')
+        .attr('cx', d => this.x_scale(d.x))
+        .attr('cy', d => this.y_scale(d.y))
+        .attr('r', d => d.size)
+        // .attr('stroke-width', d => d.stroke_width)
+        .attr('fill', d => `rgb(${d.r}, ${d.g}, ${d.b})`);
+  }
 
 
 
