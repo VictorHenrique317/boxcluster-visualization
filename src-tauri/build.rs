5f60d02ca42f748a5256d381ba72c45219f4bbe4
Victor Henrique <68289673+VictorHenrique317@users.noreply.github.com>
2024-01-31 16:08:14 -0300

    Release action

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,17 +1,17 @@
 fn main() {
-  #[cfg(target_os="linux")]{
-    println!("cargo:rustc-link-lib=python3.10"); 
-    // apt install python3.10-dev
-    // export PYO3_PYTHON="/usr/bin/python3.10"
-    // export LD_LIBRARY_PATH=/usr/lib/python3.10/config-3.10-x86_64-linux-gnu
-  }
+  // #[cfg(target_os="linux")]{
+  //   println!("cargo:rustc-link-lib=python3.10"); 
+  //   // apt install python3.10-dev
+  //   // export PYO3_PYTHON="/usr/bin/python3.10"
+  //   // export LD_LIBRARY_PATH=/usr/lib/python3.10/config-3.10-x86_64-linux-gnu
+  // }
 
-  #[cfg(target_os="windows")]{
-    println!("cargo:rustc-link-lib=python310"); 
-    // Install python 3.10.11
-    // set PYO3_PYTHON="C:\\Python310\\python.exe"
-    // set LIB="C:\\Python310\\libs;%LIB%"
-  }
+  // #[cfg(target_os="windows")]{
+  //   println!("cargo:rustc-link-lib=python310"); 
+  //   // Install python 3.10.11
+  //   // set PYO3_PYTHON="C:\\Python310\\python.exe"
+  //   // set LIB="C:\\Python310\\libs;%LIB%"
+  // }
 
   tauri_build::build()
 }
