da65a933bc94b4694522c0c51389f13a0211e375
Victor <victor.henrique5800@gmail.com>
2024-04-01 13:01:40 -0300

    Implementing intersection details dialog

--- src/app/components/visualization/intersection-details-dialog/intersection-details-dialog.component.ts
+++ src/app/components/visualization/intersection-details-dialog/intersection-details-dialog.component.ts
@@ -6,6 +6,11 @@ import {MatSort, MatSortModule} from '@angular/material/sort';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 
+export interface IntersectedTuple{
+    tuple: String;
+    dim: String;
+}
+
 @Component({
   selector: 'app-intersection-details-dialog',
   standalone: true,
@@ -31,9 +36,11 @@ export class IntersectionDetailsDialogComponent {
   protected intersectors_data_source: MatTableDataSource<Array<number>>;
 
   protected intersector_displayed_columns: string[] = ['tuple', 'dim']
-  protected intersector_data_source: MatTableDataSource<Array<string>>;
+  protected intersector_data_source: MatTableDataSource<IntersectedTuple[]>;
 
   protected intersector_id: number;
+
+
 
   constructor(public dialogRef: MatDialogRef<IntersectionDetailsDialogComponent>, 
       @Inject(MAT_DIALOG_DATA) public data: {intersection_details: IntersectionDetails}, private cdr: ChangeDetectorRef){
@@ -57,8 +64,20 @@ export class IntersectionDetailsDialogComponent {
   }
 
   protected selectIntersector(intersector_id: number){
-    this.intersector_id = intersector_id;
-    this.intersector_data_source = new MatTableDataSource(this.intersections.get(this.intersector_id)[1]);
-    
+    this.intersector_id = parseInt(intersector_id[0]);
+
+    let intersected_dims: Array<Array<string>> = this.intersections.get(this.intersector_id)[1];
+
+    let i = 0;
+    let intersector_data_source: Array<IntersectedTuple> = [];
+    intersected_dims.forEach(dim => {
+      let values: Array<String> = dim.flat();
+      for (let j = 0; j < values.length; j++){
+        intersector_data_source.push({tuple: values[j], dim: 'DIM' + i});
+      }
+      i++;
+    });
+
+    this.intersector_data_source = new MatTableDataSource(Array.from(intersector_data_source, x => [x]));
   }
 }
